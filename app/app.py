2649d338f582387d97e9a117b22004c9f872656f
Chuck146 <sam98@njit.edu>
2020-04-21 00:22:28 -0400

    Task: updated app file and imported render_template

--- app/app.py
+++ app/app.py
@@ -2,6 +2,8 @@ from typing import List, Dict
 import mysql.connector
 import simplejson as json
 from flask import Flask, Response
+from flask import render_template
+
 
 app = Flask(__name__)
 
@@ -25,8 +27,19 @@ def cities_import() -> List[Dict]:
 
     return result
 
+
 @app.route('/')
-def index() -> str:
+def index():
+    user = {'username': 'Miguel'}
+    cities_data = cities_import()
+
+    return render_template('index.html', title='Home', user=user, cities=cities_data)
+
+
+
+
+@app.route('/')
+def cities() -> str:
     js = json.dumps(cities_import())
     resp = Response(js, status=200, mimetype='application/json')
     return resp
